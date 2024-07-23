c84c416156812829f2c0dc7690af5838ff5b13a4
Daniil <danidashkevichit@gmail.com>
2024-07-23 17:07:59 +0300

    feat: add Header, SearchBar components to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,25 @@
 import { JSX } from 'react';
 import './Home.css';
+import Header from '../../components/Header/Header';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 function Home(): JSX.Element {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="wrapper">
+          <section className="main__section--search">
+            <h1>
+              let's find some <span className="text--primary">art</span> here!
+            </h1>
+            <SearchBar />
+          </section>
+        </div>
+      </main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default Home;
