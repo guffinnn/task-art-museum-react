deef9cd204cb90a96863d17b74193e2de40f84b0
Daniil <danidashkevichit@gmail.com>
2024-07-26 11:22:11 +0300

    feat: add sorting in FavoriteCardList

--- src/components/FavoriteCardList/FavoriteCardList.tsx
+++ src/components/FavoriteCardList/FavoriteCardList.tsx
@@ -1,13 +1,15 @@
-import { JSX, useEffect, useState } from 'react';
+import { JSX, useEffect, useMemo, useState } from 'react';
 import { CardImageSmall, CardListWrapper } from '../SmallCardList/styled';
 import { Loader } from './styled';
 import { URL_IMAGE } from '../../constants/api';
-import SmallCard from '../SmallCard/SmallCard';
 import { useFavorites } from '../../context/FavoritesContext';
+import SmallCard from '../SmallCard/SmallCard';
+import SortDropdown from '../SortDropdown/SortDropdown';
 
 function FavoriteCardList(): JSX.Element {
   const { favorites } = useFavorites();
   const [storageIsEmpty, setStorageIsEmpty] = useState<boolean>(true);
+  const [sortCriteria, setSortCriteria] = useState<string>('date');
 
   useEffect(() => {
     if (favorites.length > 0) {
@@ -17,18 +19,44 @@ function FavoriteCardList(): JSX.Element {
     }
   }, [favorites]);
 
+  const sortedFavorites = useMemo(() => {
+    return [...favorites].sort((a, b) => {
+      switch (sortCriteria) {
+        case 'date':
+          return (
+            new Date(a.date_end).getTime() - new Date(b.date_end).getTime()
+          );
+        case 'date_reverse':
+          return (
+            new Date(b.date_end).getTime() - new Date(a.date_end).getTime()
+          );
+        case 'alphabet':
+          return a.title.localeCompare(b.title);
+        case 'alphabet_reverse':
+          return b.title.localeCompare(a.title);
+      }
+      return 0;
+    });
+  }, [favorites, sortCriteria]);
+
   return (
     <>
       {!storageIsEmpty ? (
-        <CardListWrapper>
-          {favorites.map((item, index) => (
-            <SmallCard key={index} item={item}>
-              <CardImageSmall
-                image_url={URL_IMAGE({ imageId: item.image_id })}
-              />
-            </SmallCard>
-          ))}
-        </CardListWrapper>
+        <>
+          <SortDropdown
+            sortCriteria={sortCriteria}
+            setSortCriteria={setSortCriteria}
+          />
+          <CardListWrapper>
+            {sortedFavorites.map((item, index) => (
+              <SmallCard key={index} item={item}>
+                <CardImageSmall
+                  image_url={URL_IMAGE({ imageId: item.image_id })}
+                />
+              </SmallCard>
+            ))}
+          </CardListWrapper>
+        </>
       ) : (
         <Loader>No favorites yet.</Loader>
       )}
---
+++ src/components/SortDropdown/SortDropdown.tsx
@@ -0,0 +1,30 @@
+import { JSX, useState } from 'react';
+import './SortDropdown.css';
+
+interface SortDropdownProps {
+  sortCriteria: string;
+  setSortCriteria: (sortCriteria: string) => void;
+}
+
+function SortDropdown({
+  sortCriteria,
+  setSortCriteria,
+}: SortDropdownProps): JSX.Element {
+  return (
+    <div className="sort__dropdown">
+      <label htmlFor="sort">Sorting parameters:</label>
+      <select
+        id="sort"
+        value={sortCriteria}
+        onChange={(e) => setSortCriteria(e.target.value)}
+      >
+        <option value="date">Oldest first</option>
+        <option value="date_reverse">Newest first</option>
+        <option value="alphabet">A &gt; Z</option>
+        <option value="alphabet_reverse">Z &gt; A</option>
+      </select>
+    </div>
+  );
+}
+
+export default SortDropdown;
