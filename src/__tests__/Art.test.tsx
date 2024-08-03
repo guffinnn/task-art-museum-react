b8450c43765011988a8949e67f18557bc8801656
Daniil <danidashkevichit@gmail.com>
2024-08-03 20:01:12 +0300

    fix: change Art tests

--- src/__tests__/Art.test.tsx
+++ src/__tests__/Art.test.tsx
@@ -1,8 +1,10 @@
+import { DEFAULT_TEXT, MESSAGES } from '@constants/values';
 import { useFavorites } from '@context/FavoritesContext';
-import { ArtInfo } from '@custom-types/artInfo';
-import Art from '@pages/Art/Art';
+import { Art } from '@pages/Art/Art';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { ARTWORK_EXAMPLE } from './index';
 
 jest.mock('@context/FavoritesContext', () => ({
   useFavorites: jest.fn(),
@@ -10,31 +12,17 @@ jest.mock('@context/FavoritesContext', () => ({
 
 global.fetch = jest.fn();
 
-const mockArtwork: ArtInfo = {
-  id: 1,
-  title: 'Art Title',
-  artist_title: 'Artist Name',
-  is_public_domain: true,
-  date_start: 2020,
-  date_end: 2023,
-  place_of_origin: 'Place of Origin',
-  dimensions: '10x10',
-  credit_line: 'Credit Line',
-  image_id: 'image1',
-  theme_titles: ['Theme 1', 'Theme 2'],
-};
-
 describe('Art should', () => {
   beforeEach(() => {
     (useFavorites as jest.Mock).mockReturnValue({
-      favorites: [mockArtwork],
+      favorites: [ARTWORK_EXAMPLE],
       toggleFavorite: jest.fn(),
     });
 
     (fetch as jest.Mock).mockImplementation((url) => {
       if (url.includes('artwork')) {
         return Promise.resolve({
-          json: () => Promise.resolve({ data: mockArtwork }),
+          json: () => Promise.resolve({ data: ARTWORK_EXAMPLE }),
         });
       }
     });
@@ -53,7 +41,7 @@ describe('Art should', () => {
       </MemoryRouter>,
     );
 
-    const loadingElement = screen.getByText('Loading...');
+    const loadingElement = screen.getByText(MESSAGES.LOADING);
     expect(loadingElement).toBeDefined();
   });
 
@@ -67,36 +55,38 @@ describe('Art should', () => {
     );
 
     await waitFor(() => {
-      const titleElement = screen.getByText('Art Title');
+      const titleElement = screen.getByText(ARTWORK_EXAMPLE.title);
       expect(titleElement).toBeDefined();
     });
 
-    const artistElement = screen.getByText('Artist Name');
+    const artistElement = screen.getByText(ARTWORK_EXAMPLE.artistTitle);
     expect(artistElement).toBeDefined();
 
-    const dateElement = screen.getByText('2020–2023');
+    const dateElement = screen.getByText(
+      `${ARTWORK_EXAMPLE.dateStart}-${ARTWORK_EXAMPLE.dateEnd}`,
+    );
     expect(dateElement).toBeDefined();
 
-    const placeElement = screen.getByText('Place of Origin');
+    const placeElement = screen.getByText(ARTWORK_EXAMPLE.placeOfOrigin);
     expect(placeElement).toBeDefined();
 
-    const dimensionsElement = screen.getByText('10x10');
+    const dimensionsElement = screen.getByText(ARTWORK_EXAMPLE.dimensions);
     expect(dimensionsElement).toBeDefined();
 
-    const creditLineElement = screen.getByText('Credit Line');
+    const creditLineElement = screen.getByText(ARTWORK_EXAMPLE.creditLine);
     expect(creditLineElement).toBeDefined();
 
     const repositoryElement = screen.getByText('Theme 1, Theme 2');
     expect(repositoryElement).toBeDefined();
 
-    const publicElement = screen.getByText('Public');
+    const publicElement = screen.getByText(DEFAULT_TEXT.PUBLIC);
     expect(publicElement).toBeDefined();
   });
 
   test('toggle favorite status on button click', async () => {
     const toggleFavoriteMock = jest.fn();
     (useFavorites as jest.Mock).mockReturnValue({
-      favorites: [mockArtwork],
+      favorites: [ARTWORK_EXAMPLE],
       toggleFavorite: toggleFavoriteMock,
     });
 
---
+++ src/__tests__/index.ts
@@ -0,0 +1,15 @@
+import { ArtInfo } from '@custom-types/artInfo';
+
+export const ARTWORK_EXAMPLE: ArtInfo = {
+  id: '1',
+  title: 'Art Title',
+  artistTitle: 'Artist Name',
+  isPublicDomain: true,
+  dateStart: 2020,
+  dateEnd: 2023,
+  placeOfOrigin: 'Place of Origin',
+  dimensions: '10x10',
+  creditLine: 'Credit Line',
+  imageId: 'image1',
+  themeTitles: ['Theme 1', 'Theme 2'],
+};
